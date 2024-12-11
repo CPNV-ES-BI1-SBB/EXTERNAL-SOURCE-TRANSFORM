403f261b04228a05af1a52da301bb8aec7a21728
Sebastien-Moraz <sebastien@moraz.net>
2024-12-11 23:40:09 +0100

    test: add test for TransformBuilder

---
+++ tests/transformated_data_example.py
@@ -0,0 +1,22 @@
+def transformated_data_example():
+    return {
+  "name": "Lausanne",
+  "departures": [
+    {
+      "departureStationName": "Lausanne",
+      "destinationStationName": "Vallorbe",
+      "viaStationNames": [
+        "Prilly-Malley",
+        "Renens VD",
+        "Bussigny"
+      ],
+      "departureTime": "2024-01-12 00:02:00",
+      "train": {
+        "g": "R",
+        "l": "4",
+      },
+      "platform": "8",
+      "sector": "024492",
+    }
+  ]
+}
