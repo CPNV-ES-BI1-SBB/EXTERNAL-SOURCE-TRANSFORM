194f588dd5d6bcb1ce79e94a998363342d38772b
Sebastien-Moraz <sebastien@moraz.net>
2024-12-13 17:30:10 +0100

    feat: add implementation for DataTransformDirector

--- app/services/data_transform_director.py
+++ app/services/data_transform_director.py
@@ -1,9 +1,13 @@
+from app.services.data_transform_builder import DataTransformBuilder
+
 class DataTransformDirector:
     def __init__(self, awsProvider):
         self._awsProvider = awsProvider
 
     def clean_station_departures(self, path: str) -> dict:
-        pass
+        data = self._load(path)
+        self._dataTransformBuilder = DataTransformBuilder(data)
+        return self._dataTransformBuilder.set_initial_station().set_departures().build()
 
     def _load(self, path: str) -> dict:
-        pass
+        return self._awsProvider.download(path)
