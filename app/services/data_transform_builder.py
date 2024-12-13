25a35fec47c2b10c65f345eddb2b299eedad9199
Sebastien-Moraz <sebastien@moraz.net>
2024-12-13 17:35:09 +0100

    fix: add UnableToTransformError in DataTransformBuilder

--- app/services/data_transform_builder.py
+++ app/services/data_transform_builder.py
@@ -1,3 +1,6 @@
+from app.errors.unable_to_transform_error import UnableToTransformError
+
+
 class DataTransformBuilder:
     def __init__(self, data):
         self._input = data
@@ -11,24 +14,30 @@ class DataTransformBuilder:
         return self
 
     def set_departures(self):
-        self._output["departures"] = [
-            self._build_departure(connection) for connection in self._input["connections"]
-        ]
-        return self
+        try:
+            self._output["departures"] = [
+                self._build_departure(connection) for connection in self._input["connections"]
+            ]
+            return self
+        except KeyError:
+            raise UnableToTransformError()
 
     def _build_departure(self, connection) -> dict:
-        departure = {
-            "departureStationName": self._input["stop"]["name"],
-            "destinationStationName": connection["terminal"]["name"],
-            "viaStationNames": [stop["name"] for stop in connection["subsequent_stops"]],
-            "departureTime": connection["time"],
-            "train": {
-                "g": connection["*G"],
-                "l": connection["*L"],
-            },
-        }
-        departure["platform"], departure["sector"] = self._split_track(connection["track"])
-        return departure
+        try:
+            departure = {
+                "departureStationName": self._input["stop"]["name"],
+                "destinationStationName": connection["terminal"]["name"],
+                "viaStationNames": [stop["name"] for stop in connection["subsequent_stops"]],
+                "departureTime": connection["time"],
+                "train": {
+                    "g": connection["*G"],
+                    "l": connection["*L"],
+                },
+            }
+            departure["platform"], departure["sector"] = self._split_track(connection["track"])
+            return departure
+        except KeyError:
+            raise UnableToTransformError()
 
     @staticmethod
     def _split_track(track) -> tuple:
